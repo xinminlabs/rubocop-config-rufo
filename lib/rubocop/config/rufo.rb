b2c7c4fee2012335fab45c56b53d73394d8e9ebf
Richard Huang <flyerhzm@gmail.com>
2019-01-08 21:36:57 +0800

    first commit

---
+++ lib/rubocop/config/rufo.rb
@@ -0,0 +1,10 @@
+require "rubocop/config/rufo/version"
+
+module Rubocop
+  module Config
+    module Rufo
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rubocop/config/rufo/version.rb
@@ -0,0 +1,7 @@
+module Rubocop
+  module Config
+    module Rufo
+      VERSION = "0.1.0"
+    end
+  end
+end
